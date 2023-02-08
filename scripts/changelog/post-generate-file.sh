e8c4d988ee8b82cbf45508e97b944c20044b09d7
mizushima1226 <mizuki.bz.3224@gmail.com>
2023-02-08 16:43:41 +0900

    fix

--- scripts/changelog/post-generate-file.sh
+++ scripts/changelog/post-generate-file.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-# Changelog生成のための一時ファイル(gitignore)
-mkdir generate
-
-echo $1 > generate/latest_changelog.json
-
 # 過去のstgの目印は除去
 cat changelog.json | jq '.[] | select(.is_staging | not)' | jq -s . > generate/current.json
 jq -s '.[0] + .[1]' generate/latest_changelog.json generate/current.json > generate/pre_changelog.json
